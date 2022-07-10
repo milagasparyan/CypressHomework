d0acdf9ee76739baecef1cc57683003884d3e3ed
milagasparyan <103820971+milagasparyan@users.noreply.github.com>
2022-07-10 20:46:08 +0400

    Add files via upload

---
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -0,0 +1,25 @@
+
+describe('example to-do app', () => {
+  it('visits saucedemo', () => {
+      Cypress.on("uncaught:exception", () => {
+          return false;
+      });
+        cy.visit('https://www.saucedemo.com/')
+      })
+
+      it('account login', () => {
+        cy.get('[id="user-name"]').type('standard_user')
+        cy.get('[id="password"]').type('secret_sauce')
+        cy.get('[id="login-button"]').click()
+        cy.get('[class="shopping_cart_link"]').should('be.visible')
+        cy.get('[id="add-to-cart-sauce-labs-backpack"]').click()
+        cy.get('[class="shopping_cart_link"]').click()
+        cy.get('[class="inventory_item_name"]').should('be.visible')
+        cy.get('[id="remove-sauce-labs-backpack"]').click()
+        cy.get('[class="removed_cart_item"]').should('exist')
+      })
+})
+
+
+
+
